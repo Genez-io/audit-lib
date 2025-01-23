1b37c67b1f4d0f19b6707c8fd87e78c340e647c6
Cioban George-Adrian <cgeorgeadrian99@gmail.com>
2025-01-23 14:12:21 +0200

    Add generic toString func

--- audit/resource.go
+++ audit/resource.go
@@ -1,6 +1,9 @@
 package audit
 
 import (
+	"fmt"
+	"strings"
+
 	auditmodels "github.com/Genez-io/audit-lib/audit_models"
 )
 
@@ -13,7 +16,7 @@ type BaseProjectLevelAudit struct {
 	ownerId      *string
 	// Note this should be populated when calling SubmitAuditLog.
 	parentAuditLogId string
-	actionTaken 		auditmodels.Action
+	actionTaken      auditmodels.Action
 }
 
 type BaseAccountLevelAudit struct {
@@ -24,11 +27,58 @@ type BaseAccountLevelAudit struct {
 	ownerId      *string
 	// Note this should be populated when calling SubmitAuditLog.
 	parentAuditLogId string
-	actionTaken 		auditmodels.Action
+	actionTaken      auditmodels.Action
 }
 
 type ResourceAudit interface {
 	SubmitAuditLog(action auditmodels.Action) error
 	SubmitAuditDetail(message string) error
 	ToString() string
+}
+
+func AuditLogToString(resourceType, resourceName, action string) string {
+	var message string
+
+	singularResource := resourceType[:len(resourceType)-1]
+	singularCamelCaseResource := strings.ToUpper(string(resourceType[:1])) + string(resourceType[1:len(resourceType)-1])
+	switch action {
+	case string(auditmodels.ActionCreate):
+		if resourceName == "" {
+			message = fmt.Sprintf("Created a new %s", singularResource)
+		} else {
+			message = fmt.Sprintf("%s %s was created", singularCamelCaseResource, resourceName)
+		}
+	case string(auditmodels.ActionUpdate):
+		if resourceName == "" {
+			message = fmt.Sprintf("Updated a %s", singularResource)
+		} else {
+			message = fmt.Sprintf("%s %s was updated", singularCamelCaseResource, resourceName)
+		}
+	case string(auditmodels.ActionDelete):
+		if resourceName == "" {
+			message = fmt.Sprintf("Deleted a %s", singularResource)
+		} else {
+			message = fmt.Sprintf("%s %s was deleted", singularCamelCaseResource, resourceName)
+		}
+	case string(auditmodels.ActionDisable):
+		if resourceName == "" {
+			message = fmt.Sprintf("Disabled a %s", singularResource)
+		} else {
+			message = fmt.Sprintf("%s %s was disabled", singularCamelCaseResource, resourceName)
+		}
+	case string(auditmodels.ActionEnable):
+		if resourceName == "" {
+			message = fmt.Sprintf("Enabled a %s", singularResource)
+		} else {
+			message = fmt.Sprintf("%s %s was enabled", singularCamelCaseResource, resourceName)
+		}
+	case string(auditmodels.ActionLogin):
+		return "User logged in"
+	case string(auditmodels.ActionLogout):
+		return "User logged out"
+	default:
+		message = fmt.Sprintf("Invalid action: %s", action)
+	}
+
+	return message
 }
